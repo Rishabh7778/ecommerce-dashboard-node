ebbdbfd62d2349bdf088ebf02344586a8ad7440a
RAVI KUMAR <131015092+Ravikumar00721@users.noreply.github.com>
2026-05-11 19:21:35 +0530

    fix render

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,14 +1,15 @@
 import mysql from 'mysql2/promise'; // Seedha promise wala version use karein
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '', // Agar password hai toh yahan likhein
-  database: 'reactecommerce',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: Number(process.env.DB_PORT),
+
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
 
-// Yahan 'export default' hona zaroori hai tabhi 'import db' kaam karega
 export default pool;
